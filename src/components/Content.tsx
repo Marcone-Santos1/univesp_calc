c6fc9bccb72498dd7127406bd8b34505ba1bc25d
Marcone-Santos <marconemagalhaes203@gmail.com>
2025-10-07 21:57:55 -0300

    seo

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,10 +2,10 @@ export const Content = () => {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900 py-16 px-4">
       <div className="max-w-4xl mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-8 text-gray-800 dark:text-gray-200">Como funciona</h2>
+        <h2 className="text-3xl font-bold text-center mb-8 text-gray-800 dark:text-gray-200">Como funciona e como calcular sua média na Univesp</h2>
         <div className="grid md:grid-cols-2 gap-8">
           <div className="p-6 bg-blue-50 dark:bg-gray-800 rounded-lg">
-            <h3 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200">Calculadora de Média
+            <h3 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200">Calculadora Univesp de Média
               Final</h3>
             <p className="text-gray-700 dark:text-gray-400 mb-4">Quer saber sua média final com base nas
               notas do
@@ -31,7 +31,7 @@ export const Content = () => {
           </div>
 
           <div className="p-6 bg-blue-50 dark:bg-gray-800 rounded-lg">
-            <h3 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200">Cálculo da Média com
+            <h3 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200">Cálculo da Média Univesp com
               Exame</h3>
             <p className="text-gray-700 dark:text-gray-400 mb-4">Se sua instituição permite exames finais de
               recuperação,
